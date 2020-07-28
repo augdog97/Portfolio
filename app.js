4db288bba161a16429b1974de09b0f919c164b16
augdog97 <augdog@me.com>
2020-07-28 16:00:10 -0400

    Serving HTML files

--- app.js
+++ app.js
@@ -9,24 +9,24 @@ const indexRouter = require('./routes/index');
 const contactRouter = require('./routes/contact');
 const timeRouter = require('./routes/time');
 const portRouter = require('./routes/portfolio');
+
 var app = express();
+
+app.use(express.static( 'public'));
 
 
 
 app.use('/', indexRouter);
-app.use('/html-files/portfolio.html/', portRouter);
-app.use('/html-files/time.html/', timeRouter);
-app.use('/html-files/contact.html', contactRouter);
+app.use('/', portRouter);
+app.use('/', timeRouter);
+app.use('/', contactRouter);
 
 app.use(express.urlencoded({extended: true}));
 
-
-
-
-
-
 app.get('*',(req,res) => { 
   res.sendStatus(404);
+  console.log('try again!');
+  
 })
 
 
--- routes/contact.js
+++ routes/contact.js
@@ -6,8 +6,8 @@ var path = require('path');
 
 
 
-contact.use(express.static('public'));
 
+contact.use(express.static(__dirname + 'portfolio'));
 
 contact.get('/html-files/contact.html/',  (req,res,next) => {
   res.sendFile('/Users/AugustShah 1/Documents/Coding-Tools/Projects/portfolio/portfolio/html-files/contact.html');
@@ -19,85 +19,5 @@ contact.get('/html-files/contact.html/',  (req,res,next) => {
 
 
 
-/*contact.post('/contact.html#contact', (req, res) => {
-    const { name, email, message } = req.body;
-    log('Data: ', req.body);
-
-    sendMail(name, email, message, function(err, data) {
-        if (err) {
-            log('ERROR: ', err);
-            return res.status(500).json({ message: err.message || 'Internal Error' });
-        }
-        log('Email sent!!!');
-        return res.json({ message: 'Email sent!!!!!' });
-    });
-});
-   
-
-const nodemailer = require('nodemailer');
-const mailGun = require('nodemailer-mailgun-transport');
-
-const auth = {
-    auth: {
-        api_key: '8caaa273a49095af3afcc63af5777b8d-ffefc4e4-d83dea73',
-        domain: 'sandbox53e2a76bde1344a7a9cbb1fd5e6c2994.mailgun.org'
-    }
-};
-
-const transporter = nodemailer.createTransport(mailGun(auth));
-
-
-const sendMail = (email, subject, text, cb) => {
-    const mailOptions = {
-        from: email, 
-        to: 'augustshah@02pilot.com', 
-        name,
-        message
-    };
-
-    transporter.sendMail(mailOptions, function (err, data) {
-        if (err) {
-            return cb(err, null);
-        }
-        return cb(null, data);
-    });
-}
-
-
-POST 
-contact.post('/', function(req, res) {
-    var name = req.body["name"];
-  var mail = req.body["email"];
-  var message = req.body["message"];
-
-  if (!name || !mail) {
-    res.send("Error: Email & Subject should not be Blank");
-    return false;
-  }
-
-var smtpTransport = nodemailer.createTransport("SMTP", {
-    host: "smtp.gmail.com", 
-    secureConnection: true, 
-    port: 465, 
-        auth: {
-            user: 'augustshah@02pilot.com',
-            pass: 'tqomfprhoqautsxg'
-        }
-});
-
-var mailOptions = {
-    from: "Node Emailer - <email@gmail.com>",
-    to: req.body.email, 
-    name: req.body.name + " -",
-    html: "<b>"+req.body.message+"<b>"
-}
-smtpTransport.sendMail(mailOptions, function(error, response) {
-    if (error) {
-        res.send("Email could not be sent due to error:" +error);
-    }else {
-        res.send("Email has been sent successfully");
-    }
-});
-});*/
 
 module.exports = contact;
