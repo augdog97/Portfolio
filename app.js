35a1f1f81eb5b66aa311e095e94bda2ea69faca8
augdog97 <augdog@me.com>
2020-07-28 18:57:47 -0400

    Added routes to nav bar

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* Imports */
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -5,6 +6,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const nodemailer = require('nodemailer')
 const bodyParser = require('body-parser');
+
+/* Routers */
 const indexRouter = require('./routes/index');
 const contactRouter = require('./routes/contact');
 const timeRouter = require('./routes/time');
@@ -14,8 +17,6 @@ var app = express();
 
 app.use(express.static( 'public'));
 
-
-
 app.use('/', indexRouter);
 app.use('/', portRouter);
 app.use('/', timeRouter);
@@ -24,9 +25,7 @@ app.use('/', contactRouter);
 app.use(express.urlencoded({extended: true}));
 
 app.get('*',(req,res) => { 
-  res.sendStatus(404);
-  console.log('try again!');
-  
+  res.sendStatus(404);  
 })
 
 
